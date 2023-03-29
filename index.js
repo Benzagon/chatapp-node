0223cbc28c016eb09fdfb5d530caf89e1ac432ff
Gonzalo Benzaquen <91753390+Benzagon@users.noreply.github.com>
2023-03-29 12:05:48 -0300

    feat: getUsers() and createUser() functions
    Implemented both functions with prisma

--- index.js
+++ index.js
@@ -7,42 +7,63 @@ const mysql = require("mysql2");
 app.use(express.json());
 const port = process.env.PORT;
 
-console.log("HOLA")
+// const connection = mysql.createConnection({
+//     host: process.env.DATABASE_HOST,
+//     user:  process.env.DATABASE_USERNAME,
+//     password:  process.env.DATABASE_PASSWORD,
+//     database:  process.env.DATABASE,
+//     ssl: {
+//         rejectUnauthorized: false,
+//     },
+// });
 
-const connection = mysql.createConnection({
-    host: process.env.DATABASE_HOST,
-    user:  process.env.DATABASE_USERNAME,
-    password:  process.env.DATABASE_PASSWORD,
-    database:  process.env.DATABASE,
-    ssl: {
-        rejectUnauthorized: false,
-    },
-});
+const { PrismaClient } = require('@prisma/client') 
+const prisma = new  PrismaClient()
 
-connection.connect((err) => {
-    if(err) {
-        console.error("Error conectándose: " + err)
-        return;
-    }
-    
-    console.log("Db connected");
-});
+async function createUser(name, email, password){
+    const newUser = await prisma.user.create({
+        data: {
+            name: name,
+            email: email,
+            password: password
+        }
+    });
+};
+
+async function deleteUser(id){
+    const deletedUser = await prisma.user.delete({
+        where: {
+            id: id
+        }
+    });
+    return ("Deleted user " + id)
+};
+
+async function getUsers() {
+    const users = await prisma.user.findMany({
+        select: {
+            id: true,
+            name: true,
+            email: true
+        }
+    });
+    return users;
+};
+
+//createUser("Nai", "nai@gmail.com", "nailope").then((a) => console.log("Created user")).catch((e) => console.error(e));
+
+//deleteUser(2).then((a) => console.log(a)).catch((e) => console.error(e));
+
+//getUsers().then((a) => console.log(a)).catch((e) => console.error(e));
 
 app.get("/", (req, res) => {
     res.send("Api running OK...");
 });
 
 app.get("/users", (req, res) => {
-    connection.query("SELECT * FROM users", (err, rows) => {
-        if(err){
-            console.error("Error consultando: " + err); 
-        }
-
-        console.log(rows);
-        res.send(rows);
-    })
-})
+    getUsers().then((a) => res.send(a)).catch((e) => console.error(e));
+});
 
 app.listen(port, () => {
     console.log(`> app listening on port ${port}`)
-})
+});
